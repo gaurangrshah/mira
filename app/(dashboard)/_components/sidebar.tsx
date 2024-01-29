cff1fb5d1fd4f7d6f8c30616bfdd1f6ccf885ad0
gshah2020 <gaurang.r.shah@gmail.com>
2024-01-29 18:46:43 -0500

    styles: add default dashboard layout

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,5 @@
+export function Sidebar() {
+  return (
+    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex px-3 flex-col gap-y-4 text-white">sidebar</aside>
+  )
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
       <div>
-        This is authenticated content.
-      </div>
-      <div>
-        <UserButton/>
+        Dashboard
       </div>
     </div>
   );
