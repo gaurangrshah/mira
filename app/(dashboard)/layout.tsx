113c17599437bd51b4948d50f2c194663cd905ce
gshah2020 <gaurang.r.shah@gmail.com>
2024-01-30 01:19:54 -0500

    fix: dashboard % flex height

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({children}: DashboardLayoutProps) {
   return (
     <main className="h-full">
       <Sidebar/>
-      <div className="pl-[60px]">
+      <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
           <OrgSidebar/>
           <div className="h-full flex-1">
