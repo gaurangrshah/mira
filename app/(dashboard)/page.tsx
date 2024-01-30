90e657ddef94a23bef07edc993cecf1e0ba88222
gshah2020 <gaurang.r.shah@gmail.com>
2024-01-30 01:20:09 -0500

    feat: empty dashboard states

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,37 @@
+"use client";
 
+import { useOrganization } from "@clerk/nextjs";
 
-export default function Home() {
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+import { query } from '../../convex/_generated/server';
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string; // search = query string from file:///./_components/search-input.tsx
+    favorites?: string; // favorites = href from file:///./_components/org-sidebar.tsx
+  };
+};
+
+const DashboardPage = ({
+  searchParams,
+}: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <div>
-        Dashboard
-      </div>
+    <div className="flex-1 h-[calc(100%-80px)] p-6"
+      // navbar height is 80px file:///./_components/navbar.tsx
+    >
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList
+          orgId={organization.id}
+          query={searchParams}
+        />
+      )}
     </div>
-  );
-}
+   );
+};
+
+export default DashboardPage;
