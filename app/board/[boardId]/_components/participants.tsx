f8065fe6b47959695b1c5371e9ff0f3f148127a7
gshah2020 <gaurang.r.shah@gmail.com>
2024-01-31 02:14:35 -0500

    feat: render participant avatar ui

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,6 +1,47 @@
+"use client";
+import { UserAvatar } from "@/app/board/[boardId]/_components/user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
+import { useOthers, useSelf } from "@/liveblocks.config";
+
+
+const MAX_SHOWN_USERS = 1; // number of users shown in the participants list - excluding YOU.
+
 export function Participants() {
+
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
-    <div className="absolute top-2 right-2 bg-white rounded-md p-3 h-12 flex items-center shadow-md">TODO: Information about the board</div>
+    <div className="absolute top-2 right-2 bg-white rounded-md p-3 h-12 flex items-center shadow-md">
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USERS).map(({connectionId, info}) => (
+          <UserAvatar
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name[0]}
+            borderColor={connectionIdToColor(connectionId)}
+          />
+        ))}
+        {currentUser && (
+          <UserAvatar
+            src={currentUser.info?.picture}
+            name={currentUser.info?.name}
+            fallback={currentUser.info?.name[0]}
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+          />
+        )}
+        {hasMoreUsers && (
+          <UserAvatar
+            src="/more.svg"
+            name="More"
+            fallback={`+${users.length - MAX_SHOWN_USERS}`}
+
+          />
+        )}
+      </div>
+    </div>
   )
 }
 
