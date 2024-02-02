0d9ab7cd43f812b7fcc16e3e81b4e7ec7dc179cd
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-02 00:16:07 -0500

    chore: configure liveblocks storage options

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,5 +1,12 @@
-import { createClient } from '@liveblocks/client';
+import {
+  createClient,
+  LiveList,
+  LiveMap,
+  LiveObject,
+} from '@liveblocks/client';
 import { createRoomContext } from '@liveblocks/react';
+
+import type { Layer, Color } from './types/canvas';
 
 const client = createClient({
   throttle: 16, // throttles the cursor movement in the canvas
@@ -12,6 +19,7 @@ const client = createClient({
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
   cursor: { x: number; y: number } | null;
+  selection: string[];
   // ...
 };
 
@@ -20,8 +28,8 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-  // author: LiveObject<{ firstName: string, lastName: string }>,
-  // ...
+  layers: LiveMap<string, LiveObject<Layer>>;
+  layerIds: LiveList<string>;
 };
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
